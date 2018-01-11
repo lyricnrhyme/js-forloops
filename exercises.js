6e2eb2844b03b7f677981710af58dd45ba9eb168
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-01-10 21:14:02 -1000

    Completed Exercise 4

--- exercises.js
+++ exercises.js
@@ -61,11 +61,16 @@ function leaders(person){
 }
 leaders(presidents);
 
-
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
+
+var stringOfNumbers = "";
+
+for (i = 10; i <= 20; i++) {
+	console.log(stringOfNumbers += i);
+}
 
 
 /* 5) Even Stevens
